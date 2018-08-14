9e10edd112b019947a2a802b2ff2f2c0304886d0
Sascha Gottfried <s.gottfried@hhpberlin.de>
2018-08-14 13:49:31 +0200

    TravisCI builds : update to Open MPI 3.0.2

--- install-ompi.sh
+++ install-ompi.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
-wget https://www.open-mpi.org/software/ompi/v3.0/downloads/openmpi-3.0.0.tar.gz -q -O /tmp/openmpi-3.0.0.tar.gz
+wget https://download.open-mpi.org/release/open-mpi/v3.0/openmpi-3.0.2.tar.gz -q -O /tmp/openmpi-3.0.2.tar.gz
 cd /tmp
-gunzip -c /tmp/openmpi-3.0.0.tar.gz -q| tar xf -
-cd /tmp/openmpi-3.0.0/
+gunzip -c /tmp/openmpi-3.0.2.tar.gz -q| tar xf -
+cd /tmp/openmpi-3.0.2/
 ./configure FC=gfortran-7 CC=gcc-7 --enable-mpirun-prefix-by-default --enable-static --disable-shared --quiet && make -j 4 --quiet && sudo make install --quiet
